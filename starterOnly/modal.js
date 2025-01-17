fdd0c55052685171185d7c5a3df546615125ffcc
WiloPanda <wilona.middendorp@gmail.com>
2025-01-17 01:12:26 +0100

    new functions
    function validate quantity
    function validate birthdate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,16 +17,17 @@ const form = document.querySelector("form")
 let firstName = document.getElementById("first")
 let lastName = document.getElementById("last")
 let email = document.getElementById("email")
-let date = document.getElementById("birthdate")
+let birthdate = document.getElementById("birthdate")
+let quantity = document.getElementById("quantity")
 
 // Regex validation
 let identityRegex = new RegExp("^[A-zÀ-ÿ-]+$") /*validation for first and last name*/
 let emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+") /*validation for email*/
 
-// launch modal event
+// Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
 
-// launch modal form
+// Launch modal form
 function launchModal() {
   modalbg.style.display = "block"
 }
@@ -55,6 +56,29 @@ function validateEmail(email) {
   } 
   return false
 }
+
+// Function to validate quantity
+function validateQuantity(quantity) {
+  if (quantity >= 0 && quantity <= 99) {
+    return true
+  } 
+  return false
+}
+
+// Function to validate birthdate
+function validateBirthdate(birthdate) {
+  let today = new Date()
+  let birthdateDate = new Date(birthdate)
+  let age = today.getFullYear() - birthdateDate.getFullYear()
+  let m = today.getMonth() - birthdateDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthdateDate.getDate())) {
+    age--
+  }
+  if (age >= 18) {
+    return true
+  } 
+  return false
+  }
 
 // Launch function to validate form
 form.addEventListener("submit", (event) => {
